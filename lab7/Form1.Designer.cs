d131539e3a13cbfa421610729c379ae2977c30bf
NeilArobo <n.arobo.540733@umindanao.edu.ph>
2025-05-30 19:46:26 +0800

    setting up

--- lab7/Form1.Designer.cs
+++ lab7/Form1.Designer.cs
@@ -20,20 +20,77 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnBackPage = new Button();
+            lblPageNumber = new Label();
+            btnNextPage = new Button();
+            listBoxBooks = new ListBox();
+            SuspendLayout();
+            // 
+            // btnBackPage
+            // 
+            btnBackPage.Location = new Point(220, 409);
+            btnBackPage.Name = "btnBackPage";
+            btnBackPage.Size = new Size(94, 29);
+            btnBackPage.TabIndex = 0;
+            btnBackPage.Text = "Back";
+            btnBackPage.UseVisualStyleBackColor = true;
+            // 
+            // lblPageNumber
+            // 
+            lblPageNumber.AutoSize = true;
+            lblPageNumber.Location = new Point(379, 413);
+            lblPageNumber.Name = "lblPageNumber";
+            lblPageNumber.Size = new Size(41, 20);
+            lblPageNumber.TabIndex = 1;
+            lblPageNumber.Text = "Page";
+            lblPageNumber.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // btnNextPage
+            // 
+            btnNextPage.Location = new Point(487, 409);
+            btnNextPage.Name = "btnNextPage";
+            btnNextPage.Size = new Size(94, 29);
+            btnNextPage.TabIndex = 2;
+            btnNextPage.Text = "Next";
+            btnNextPage.UseVisualStyleBackColor = true;
+            btnNextPage.Click += btnNextPage_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(12, 12);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(776, 384);
+            listBoxBooks.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBoxBooks);
+            Controls.Add(btnNextPage);
+            Controls.Add(lblPageNumber);
+            Controls.Add(btnBackPage);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnBackPage;
+        private Label lblPageNumber;
+        private Button btnNextPage;
+        private ListBox listBoxBooks;
+    }
 }
--- lab7/Form1.cs
+++ lab7/Form1.cs
@@ -1,10 +1,51 @@
 namespace lab7
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int pageSize = 10;
+        private int currentPage = 1;
+
+        public class Author
+        {
+            public int AuthorID { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookID { get; set; }
+            public string Title { get; set; }
+            public int AuthorID { get; set; }
+            public virtual Author Author { get; set; }
+        }
+
+        public async Task<List<string>> GetBooksByPageAsync(int pageNumber)
+        {
+            using (var context = new BookstoreContext)
+            {
+                var books = await context.Books
+                    .Include(b => b.author)
+                    .OrderBy(b => b.BookID)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .Select(b => $"{b.Title} by {b.Author.Name}")
+                    .ToListAsync();
+                return books;
+            }
+        }
+
+        private async void btnNextPage_Click(object sender, EventArgs e)
+        {
+            currentPage++;
+            var books = await GetBooksByPageAsync(currentPage);
+            listBoxBooks.DataSource = books;
+            lblPageNumber.Text = $"Page {currentPage}";
+        }
+    }
 }
